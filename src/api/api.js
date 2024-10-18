f3898d60f01cbe45d9af41b383eaf3cc8babe256
Kiril Kirichenko <125361474+kiri4ook@users.noreply.github.com>
2024-10-18 14:28:07 +0300

    add api annd firebase config

---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+export const sendGetRequest = async url => {
+    return await fetch(url).then(response => response.json());
+};
+
+export const questionsUrl = 'https://opentdb.com/api.php?amount=10&difficulty=easy';
